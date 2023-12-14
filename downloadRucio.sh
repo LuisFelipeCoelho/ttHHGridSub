87a063d367322257983bd34b3e2a45370c0e0787
lfaldaul <lflaldaul@lxplus.cern.ch>
2023-12-14 10:16:57 +0100

    fix mistakes

--- downloadRucio.sh
+++ downloadRucio.sh
@@ -1,122 +1,122 @@
 # === ttHH all had ===
 
 # MC20a - r14859
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttHH user.lfaldaul.2.523074.MGPy8EG_A14NNPDF23LO_ttHH_fullhad.e8531_a907_r14859_p5855
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttHH user.lfaldaul.003.523074.MGPy8EG_A14NNPDF23LO_ttHH_fullhad.e8531_a907_r14859_p5855
 
 # MC20d - r14860
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttHH user.lfaldaul.2.523074.MGPy8EG_A14NNPDF23LO_ttHH_fullhad.e8531_a907_r14860_p5855
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttHH user.lfaldaul.003.523074.MGPy8EG_A14NNPDF23LO_ttHH_fullhad.e8531_a907_r14860_p5855
 
 # MC20e - r14861
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttHH user.lfaldaul.2.523074.MGPy8EG_A14NNPDF23LO_ttHH_fullhad.e8531_a907_r14861_p5855
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttHH user.lfaldaul.003.523074.MGPy8EG_A14NNPDF23LO_ttHH_fullhad.e8531_a907_r14861_p5855
 
 # === ttHH semi lep ===
 
 # MC20a - r14859
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttHH user.lfaldaul.2.523074.MGPy8EG_A14NNPDF23LO_ttHH_semilep.e8531_a907_r14859_p5855
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttHH user.lfaldaul.003.523072.MGPy8EG_A14NNPDF23LO_ttHH_semilep.e8531_a907_r14859_p5855
 
 # MC20d - r14860
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttHH user.lfaldaul.2.523074.MGPy8EG_A14NNPDF23LO_ttHH_semilep.e8531_a907_r14860_p5855
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttHH user.lfaldaul.003.523072.MGPy8EG_A14NNPDF23LO_ttHH_semilep.e8531_a907_r14860_p5855
 
 # MC20e - r14861
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttHH user.lfaldaul.2.523074.MGPy8EG_A14NNPDF23LO_ttHH_semilep.e8531_a907_r14861_p5855
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttHH user.lfaldaul.003.523072.MGPy8EG_A14NNPDF23LO_ttHH_semilep.e8531_a907_r14861_p5855
 
 # === ttHH di lep ===
 
 # MC20a - r14859
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttHH user.lfaldaul.2.523074.MGPy8EG_A14NNPDF23LO_ttHH_dilep.e8531_a907_r14859_p5855
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttHH user.lfaldaul.003.523073.MGPy8EG_A14NNPDF23LO_ttHH_dilep.e8531_a907_r14859_p5855
 
 # MC20d - r14860
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttHH user.lfaldaul.2.523074.MGPy8EG_A14NNPDF23LO_ttHH_dilep.e8531_a907_r14860_p5855
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttHH user.lfaldaul.003.523073.MGPy8EG_A14NNPDF23LO_ttHH_dilep.e8531_a907_r14860_p5855
 
 # MC20e - r14861
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttHH user.lfaldaul.2.523074.MGPy8EG_A14NNPDF23LO_ttHH_dilep.e8531_a907_r14861_p5855
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttHH user.lfaldaul.003.523073.MGPy8EG_A14NNPDF23LO_ttHH_dilep.e8531_a907_r14861_p5855
 
 # === ttbar no all had ===
 
 # MC20a - r13167
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttbar user.lfaldaul.2.410470.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.e6337_s3681_r13167_p5855_TREE
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttbar user.lfaldaul.003.410470.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.e6337_s3681_r13167_p5855_TREE
 
 # MC20d - r13144
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttbar user.lfaldaul.2.410470.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.e6337_s3681_r13144_p5855_TREE
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttbar user.lfaldaul.003.410470.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.e6337_s3681_r13144_p5855_TREE
 
 # MC20e - r13145
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttbar user.lfaldaul.2.410470.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.e6337_s3681_r13145_p5855_TREE
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttbar user.lfaldaul.003.410470.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.e6337_s3681_r13145_p5855_TREE
 
 # MC23 - WIP
 
 # === ttbar all had ===
 
 # MC20a - r13167
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttbar user.lfaldaul.2.410470.PhPy8EG_A14_ttbar_hdamp258p75_allhad.e6337_s3681_r13167_p5855_TREE
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttbar user.lfaldaul.003.410471.PhPy8EG_A14_ttbar_hdamp258p75_allhad.e6337_s3681_r13167_p5855_TREE
 
 # MC20d - r13144
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttbar user.lfaldaul.2.410470.PhPy8EG_A14_ttbar_hdamp258p75_allhad.e6337_s3681_r13144_p5855_TREE
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttbar user.lfaldaul.003.410471.PhPy8EG_A14_ttbar_hdamp258p75_allhad.e6337_s3681_r13144_p5855_TREE
 
 # MC20e - r13145
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttbar user.lfaldaul.2.410470.PhPy8EG_A14_ttbar_hdamp258p75_allhad.e6337_s3681_r13145_p5855_TREE
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttbar user.lfaldaul.003.410471.PhPy8EG_A14_ttbar_hdamp258p75_allhad.e6337_s3681_r13145_p5855_TREE
 
 # === ttH all had ===
 
 # MC20a - r13167
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttH user.lfaldaul.2.346343.PhPy8EG_A14NNPDF23_NNPDF30ME_ttH125_allhad.e7148_s3681_r13144_p5631
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttH user.lfaldaul.003.346343.PhPy8EG_A14NNPDF23_NNPDF30ME_ttH125_allhad.e7148_s3681_r13144_p5631
 
 # MC20d - r13144
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttH user.lfaldaul.2.346343.PhPy8EG_A14NNPDF23_NNPDF30ME_ttH125_allhad.e7148_s3681_r13145_p5631
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttH user.lfaldaul.003.346343.PhPy8EG_A14NNPDF23_NNPDF30ME_ttH125_allhad.e7148_s3681_r13145_p5631
 
 # MC20e - r13145
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttH user.lfaldaul.2.346343.PhPy8EG_A14NNPDF23_NNPDF30ME_ttH125_allhad.e7148_s3681_r13167_p5631
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttH user.lfaldaul.003.346343.PhPy8EG_A14NNPDF23_NNPDF30ME_ttH125_allhad.e7148_s3681_r13167_p5631
 
 # === ttH single lep ===
 
 # MC20a - r13167
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttH user.lfaldaul.2.346344.PhPy8EG_A14NNPDF23_NNPDF30ME_ttH125_semilep.e7148_s3681_r13144_p5631
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttH user.lfaldaul.003.346344.PhPy8EG_A14NNPDF23_NNPDF30ME_ttH125_semilep.e7148_s3681_r13144_p5631
 
 # MC20d - r13144
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttH user.lfaldaul.2.346344.PhPy8EG_A14NNPDF23_NNPDF30ME_ttH125_semilep.e7148_s3681_r13145_p5631
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttH user.lfaldaul.003.346344.PhPy8EG_A14NNPDF23_NNPDF30ME_ttH125_semilep.e7148_s3681_r13145_p5631
 
 # MC20e - r13145
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttH user.lfaldaul.2.346344.PhPy8EG_A14NNPDF23_NNPDF30ME_ttH125_semilep.e7148_s3681_r13167_p5631
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttH user.lfaldaul.003.346344.PhPy8EG_A14NNPDF23_NNPDF30ME_ttH125_semilep.e7148_s3681_r13167_p5631
 
 # === ttH dilep ===
 
 # MC20a - r13167
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttH user.lfaldaul.2.346345.PhPy8EG_A14NNPDF23_NNPDF30ME_ttH125_dilep.e7148_s3681_r13144_p5631
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttH user.lfaldaul.003.346345.PhPy8EG_A14NNPDF23_NNPDF30ME_ttH125_dilep.e7148_s3681_r13144_p5631
 
 # MC20d - r13144
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttH user.lfaldaul.2.346345.PhPy8EG_A14NNPDF23_NNPDF30ME_ttH125_dilep.e7148_s3681_r13145_p5631
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttH user.lfaldaul.003.346345.PhPy8EG_A14NNPDF23_NNPDF30ME_ttH125_dilep.e7148_s3681_r13145_p5631
 
 # MC20e - r13145
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttH user.lfaldaul.2.346345.PhPy8EG_A14NNPDF23_NNPDF30ME_ttH125_dilep.e7148_s3681_r13167_p5631
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttH user.lfaldaul.003.346345.PhPy8EG_A14NNPDF23_NNPDF30ME_ttH125_dilep.e7148_s3681_r13167_p5631
 
 # === ttZqq ===
 
 # MC20a - r13167
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttZqq user.lfaldaul.2.410157.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttZqq.e5070_s3681_r13167_p5631
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttZqq user.lfaldaul.003.410157.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttZqq.e5070_s3681_r13167_p5631
 
 # MC20d - r13144
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttZqq user.lfaldaul.2.410157.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttZqq.e5070_s3681_r13144_p5631
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttZqq user.lfaldaul.003.410157.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttZqq.e5070_s3681_r13144_p5631
 
 # MC20e - r13145
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttZqq user.lfaldaul.2.410157.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttZqq.e5070_s3681_r13145_p5631
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttZqq user.lfaldaul.003.410157.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttZqq.e5070_s3681_r13145_p5631
 
 # === ttZZ ===
 
 # MC20a - r14859
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttZZ user.lfaldaul.2.500462.MGPy8EG_A14NNPDF23LO_ttZZ.e8041_a907_r14859_p5855
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttZZ user.lfaldaul.003.500462.MGPy8EG_A14NNPDF23LO_ttZZ.e8041_a907_r14859_p5855
 
 # MC20d - r14860
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttZZ user.lfaldaul.2.500462.MGPy8EG_A14NNPDF23LO_ttZZ.e8041_a907_r14860_p5855
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttZZ user.lfaldaul.003.500462.MGPy8EG_A14NNPDF23LO_ttZZ.e8041_a907_r14860_p5855
 
 # MC20e - r14861
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttZZ user.lfaldaul.2.500462.MGPy8EG_A14NNPDF23LO_ttZZ.e8041_a907_r14861_p5855
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttZZ user.lfaldaul.003.500462.MGPy8EG_A14NNPDF23LO_ttZZ.e8041_a907_r14861_p5855
 
 # === tttt ===
 
 # MC20a - r13167
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttt user.lfaldaul.2.700355.Sh_2211_tttt_muQHT2.e8351_s3797_r13144_p5855
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/ttt user.lfaldaul.003.700355.Sh_2211_tttt_muQHT2.e8351_s3797_r13144_p5855
 
 # MC20d - r13144
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/tttt user.lfaldaul.2.700355.Sh_2211_tttt_muQHT2.e8351_s3797_r13145_p5855
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/tttt user.lfaldaul.003.700355.Sh_2211_tttt_muQHT2.e8351_s3797_r13145_p5855
 
 # MC20e - r13145
-rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/tttt user.lfaldaul.2.700355.Sh_2211_tttt_muQHT2.e8351_s3797_r13167_p5855
+rucio download --dir /eos/home-l/lfaldaul/ttHH/NTUPLES/tttt user.lfaldaul.003.700355.Sh_2211_tttt_muQHT2.e8351_s3797_r13167_p5855
--- runGrid.sh
+++ runGrid.sh
@@ -1,4 +1,4 @@
 #lsetup rucio
 #lsetup panda
 
-easyjet-gridsubmit --mc-list MCListTest.txt --run-config ../easyjet/ttHHAnalysis/share/RunConfig-PHYSLITE-ttHH.yaml --exec ttHH-ntupler --nFiles 1 --campaign 2
+easyjet-gridsubmit --mc-list MCList.txt --run-config ../easyjet/ttHHAnalysis/share/RunConfig-PHYSLITE-ttHH.yaml --exec ttHH-ntupler --nFiles 10 --campaign 003
